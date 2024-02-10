75ceea0d741a44171e5735bb7d246201e043d8eb
zareva228 <zareva228@gmail.com>
2024-02-10 22:05:30 +0200

    finished working on localisation

--- db/db.py
+++ db/db.py
@@ -5,28 +5,26 @@ from pathlib import Path
 here = Path(__file__).parent
 dbPath=here/"botdb.sqlite"
 #locale
-
 imgres = Path.glob(here.parent/"getimg"/"img", "overlay-??.png")
+#overlay images gotta be strictly in format overlay-**.png, otherwise it won't work 
+#you can control number bot of locales by adding\deleting overlay pictures, 
+#^although if no additional locale strings added, then english text will be returned by default
 
-
-d = { #declare dictionary with lang names and paths to overlay pics 
-
-}
-
+d = {} #declare dictionary with lang names and paths to overlay pics 
 for path in imgres: #iterate over path in img directory to append into list 
     d[str(path.name)[8:10]]=str(path)
 
+#logic
 async def new_db():
     #create tables, ChatSettings is to store chats where bot were added and store 
     #overlay localisation ID(lang_id), which then can be joined on LangDef, where 
     #detailed Language name and path to overlay is located. 
     con = sql.connect(dbPath) #db handle
     cur = con.cursor() #cur handle
-    cur.execute("CREATE TABLE IF NOT EXISTS ChatSettings(id INTEGER PRIMARY KEY AUTOINCREMENT, chat_id INTEGER, lang_id INTEGER)")
-    cur.execute("CREATE TABLE IF NOT EXISTS LangDef(id INTEGER PRIMARY KEY AUTOINCREMENT, LangName TEXT, ovPath TEXT)")
-
-    for keys, values in d.items(): #insert data into lang table about out languages and path to Ov picture
+    cur.execute("CREATE TABLE IF NOT EXISTS ChatSettings(id INTEGER PRIMARY KEY AUTOINCREMENT, chat_id INTEGER, lang_id INTEGER)") #table for chat settings
+    cur.execute("CREATE TABLE IF NOT EXISTS LangDef(id INTEGER PRIMARY KEY AUTOINCREMENT, LangName TEXT, ovPath TEXT)")  #table for languages and paths to overlays
+    
+    for keys, values in d.items(): #insert data into lang table about languages and path to overlay image
         cur.execute("INSERT INTO LangDef(LangName, ovPath) VALUES(?,?)", (keys,values))
-
     cur.close()
     con.commit()  #write to db 🔥
--- main.py
+++ main.py
@@ -3,63 +3,51 @@
 #take banned user's pfp and return it with "eliminate" caption
 #--init
 import asyncio
+from pathlib import Path
 import os
 #aiogram imports
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message, ChatMemberUpdated, FSInputFile
-from aiogram.utils.markdown import hbold
+from aiogram.utils.i18n import I18nMiddleware
 from aiogram.filters.chat_member_updated import \
     ChatMemberUpdatedFilter, KICKED, LEFT, MEMBER, \
-    RESTRICTED, ADMINISTRATOR, CREATOR, IS_NOT_MEMBER, IS_MEMBER 
+    RESTRICTED, ADMINISTRATOR, CREATOR, IS_NOT_MEMBER, IS_MEMBER
 #custom modules import
 import config as cfg
 import getphid
+from lang.strings import GetMsg 
 from db import db 
 
 bot=Bot(token=cfg.TOKEN)
 dp = Dispatcher()
 script_folder = os.path.dirname(os.path.abspath(__file__))
-
 #--init
 
 async def database():
     await db.new_db()
     print("db initalized..")
 
-
 ########### main logic begin
-@dp.message(Command(commands=["start", "go", "begin", "run", "wake up" ])) #hewwo uwu
+@dp.message(Command(commands=["go", "begin", "run", "wake up", "start"])) #hewwo uwu
 async def start(message: Message):
-    print(message)
-    await message.answer(f"Hi! Please, add this bot into any group chat and make it an admin!")
+    msg = await GetMsg(message.from_user.language_code, "hi")
+    await message.answer(msg)
 
 @dp.message(Command(commands=["?", "whatdoido","halp", "help",]))#hewp uwu
 async def help(message: Message):
-    print(message)
-    await message.answer(f"Stop it. Get some help.")
+    msg = await GetMsg(message.from_user.language_code, "help")
+    await message.answer(msg)
 
-@dp.my_chat_member(
-    ChatMemberUpdatedFilter(member_status_changed=IS_MEMBER >> ADMINISTRATOR))
+@dp.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_MEMBER >> ADMINISTRATOR))
 async def bot_added_as_admin(event: ChatMemberUpdated):
-    #Bot added as admin, so send message
-    await event.answer(
-        text=f"Привет! Спасибо, что добавили меня в чат "
-             f"как администратора."
-             f"Я буду следить за забаненными пользователями и делать смешно."
-    )
+    await event.answer("a)")
 
-@dp.my_chat_member(
-    ChatMemberUpdatedFilter(
-        member_status_changed=IS_NOT_MEMBER >> MEMBER))
+@dp.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_NOT_MEMBER >> MEMBER))
 async def bot_added_as_member(event: ChatMemberUpdated):
     chat_info = await bot.get_chat(event.chat.id)
     if chat_info.permissions.can_send_messages:
-        await event.answer(
-            text=f"Спасибо, что добавили меня в "
-                 f'"{event.chat.title}"'
-                 f"как обычного участника. Бот требудет права администратора в чате для корректной работы. "
-        )
+        await event.answer("a)")
 
 @dp.chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_MEMBER >> KICKED )) #monitor kicked users
 async def member_kicked(event: ChatMemberUpdated):
@@ -69,19 +57,17 @@ async def member_kicked(event: ChatMemberUpdated):
         return 100000 #cant find userID
     else:
         ret = await getphid.find_max_pfp_size_by_uid(event.old_chat_member.user.id, event.old_chat_member.user.full_name) 
-        if ret == 3333: #user is looser, has 0 pfps 
-            await bot.send_message(chat_id=cid, text='User does not have any profile pictures. Le sad 🤡🤮🤡.')
-        else:
-            FSOut = FSInputFile(ret)
-            await bot.send_photo(chat_id=cid, photo=FSOut)
+        FSOut = FSInputFile(ret)
+        await bot.send_photo(chat_id=cid, photo=FSOut)
 ########### main logic end 
 
 #starter
 async def main():
     await bot.delete_webhook(drop_pending_updates=True) 
     await database()
+    print ("bot initialized, started polling")
     await dp.start_polling(bot)
-    
+
 if __name__=="__main__":
     asyncio.run(main())
 #starter
