dd95d2024ed167f798ddc1dac7bba263e978f381
zareva228 <zareva228@gmail.com>
2024-02-12 21:10:43 +0200

    finished language choosing feature & keyboard stuff

--- db/db.py
+++ db/db.py
@@ -23,8 +23,38 @@ async def new_db():
     cur = con.cursor() #cur handle
     cur.execute("CREATE TABLE IF NOT EXISTS ChatSettings(id INTEGER PRIMARY KEY AUTOINCREMENT, chat_id INTEGER, lang_id INTEGER)") #table for chat settings
     cur.execute("CREATE TABLE IF NOT EXISTS LangDef(id INTEGER PRIMARY KEY AUTOINCREMENT, LangName TEXT, ovPath TEXT)")  #table for languages and paths to overlays
-    
     for keys, values in d.items(): #insert data into lang table about languages and path to overlay image
         cur.execute("INSERT INTO LangDef(LangName, ovPath) VALUES(?,?)", (keys,values))
     cur.close()
     con.commit()  #write to db 🔥
+
+async def getOV_path(lang_code):
+    #^func to get overlay image path by given language code on demand
+    con = sql.connect(dbPath)
+    cur = con.cursor()
+    cur.execute(f"SELECT ovPath FROM LangDef WHERE LangName COLLATE NOCASE = {lang_code}")
+    ret = cur.fetchall()
+    return ret
+
+def getall_lc(): #return an array\list\dict(dunno yet) of available languages
+    con = sql.connect(dbPath)
+    cur = con.cursor()
+    cur.execute("SELECT LangName FROM LangDef")
+    ret=[]
+    for row in cur:
+        ret.append(row[0])
+    con.close()
+    return ret
+    
+async def save_chat(chat_id, lang_code): #savesettings for chat
+    con = sql.connect(dbPath)
+    cur = con.cursor()
+    cur.execute (F"SELECT 1 FROM ChatSettings WHERE ID COLLATE NOCASE = {chat_id}")
+    cur.execute (f"SELECT id FROM LangDef WHERE LangName COLLATE NOCASE = '{lang_code}'")
+    data = cur.fetchall()
+    for row in data:
+        lang_id=row[0]
+    cur.execute(f"INSERT INTO ChatSettings(chat_id, lang_id) VALUES({chat_id},{lang_id})")
+    cur.close()
+    con.commit()
+    return 0 #all good, language setting saved
--- main.py
+++ main.py
@@ -1,73 +1,122 @@
 #bot by Otvalsky
 #Add bot to group chat, watch for bans administered, 
 #take banned user's pfp and return it with "eliminate" caption
-#--init
+
+#init begin
 import asyncio
 from pathlib import Path
 import os
 #aiogram imports
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-from aiogram.types import Message, ChatMemberUpdated, FSInputFile
+from aiogram.types import Message, ChatMemberUpdated, FSInputFile,ReplyKeyboardMarkup
 from aiogram.utils.i18n import I18nMiddleware
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiogram.filters.chat_member_updated import \
-    ChatMemberUpdatedFilter, KICKED, LEFT, MEMBER, \
+    ChatMemberUpdatedFilter,  KICKED, LEFT, MEMBER, \
     RESTRICTED, ADMINISTRATOR, CREATOR, IS_NOT_MEMBER, IS_MEMBER
 #custom modules import
-import config as cfg
-import getphid
-from lang.strings import GetMsg 
-from db import db 
+import config as cfg                #bot token and stuff
+import getphid                      #get user profile picture module
+from lang.strings import GetMsg     #locale strings
+from db import db                   #settingsDB
 
 bot=Bot(token=cfg.TOKEN)
 dp = Dispatcher()
 script_folder = os.path.dirname(os.path.abspath(__file__))
-#--init
+builder = ReplyKeyboardBuilder() #keyboard builder 
 
-async def database():
-    await db.new_db()
-    print("db initalized..")
+async def init_db():
+    if os.path.isfile(script_folder+"/db/botdb.sqlite"):
+            print("db exists..")
+            return
+    else:
+        await db.new_db()
+        print("db created..")
+
+#init end
+
 
 ########### main logic begin
-@dp.message(Command(commands=["go", "begin", "run", "wake up", "start"])) #hewwo uwu
+@dp.message(Command(commands=["go", "begin", "run", "wake up", "start"])) #start whatever
 async def start(message: Message):
-    msg = await GetMsg(message.from_user.language_code, "hi")
+    msg = await GetMsg(message.from_user.language_code, "hi") #return localized message
     await message.answer(msg)
 
-@dp.message(Command(commands=["?", "whatdoido","halp", "help",]))#hewp uwu
+
+@dp.message(Command(commands=["?", "whatdoido","halp", "help",]))#hewp uwu | there will be no help
 async def help(message: Message):
     msg = await GetMsg(message.from_user.language_code, "help")
     await message.answer(msg)
 
+
 @dp.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_MEMBER >> ADMINISTRATOR))
 async def bot_added_as_admin(event: ChatMemberUpdated):
-    await event.answer("a)")
+    chat_info = await bot.get_chat(event.chat.id)
+    msg = await GetMsg(event.from_user.language_code, "chlang") #show "shoose language" message on language of user adding the bot
+    #msg = await GetMsg(Message.forward_from_chat.language_code, "chlang")
+    if chat_info.permissions.can_send_messages:
+        lcl = db.getall_lc() #get language list from db | lcl for langcuge code list
+        # Create the keyboard
+        builder = ReplyKeyboardBuilder() #do stuff
+        for l in lcl:  #loop to construct a keyboard with available languages
+            builder.button(text=l)
+        builder.adjust(3) #make it 3 buttons on one row 
+        # Send the message with the keyboard
+        await event.answer(msg, reply_markup=builder.as_markup(resize_keyboard=True, one_time_keyboard=True),)
+        print(event)
+
 
 @dp.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_NOT_MEMBER >> MEMBER))
 async def bot_added_as_member(event: ChatMemberUpdated):
     chat_info = await bot.get_chat(event.chat.id)
+    msg = await GetMsg(event.from_user.language_code, "chlang") #show "shoose language" message on language of user adding the bot
+    #msg = await GetMsg(Message.forward_from_chat.language_code, "chlang")
     if chat_info.permissions.can_send_messages:
-        await event.answer("a)")
+        lcl = db.getall_lc() #get language list from db | lcl for langcuge code list
+        # Create the keyboard
+        for l in lcl:  #loop to construct a keyboard with available languages
+            builder.button(text=l)
+        builder.adjust(3) #make it 3 buttons on one row 
+        # Send the message with the keyboard
+        await event.answer(f"[{event.from_user.full_name}](tg://user?id={event.from_user.id}), "+msg, parse_mode="markdownV2", reply_markup=builder.as_markup(resize_keybpoard=True, one_time_keyboard=True, selective=True))
 
-@dp.chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_MEMBER >> KICKED )) #monitor kicked users
+
+@dp.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_MEMBER >> KICKED )) #monitor kicked users, send funny pic
 async def member_kicked(event: ChatMemberUpdated):
     uid = event.old_chat_member.user.id
     cid = event.chat.id
     if uid is None:
-        return 100000 #cant find userID
+        return 100000 #cant find userID | placeholder
     else:
         ret = await getphid.find_max_pfp_size_by_uid(event.old_chat_member.user.id, event.old_chat_member.user.full_name) 
         FSOut = FSInputFile(ret)
         await bot.send_photo(chat_id=cid, photo=FSOut)
+
+
+@dp.message(F.text.lower().in_({'en', 'ru', 'ua'}))
+async def echo(message: Message):
+    print(message)
+    umsg=message.text #user choice on keyboard  
+    msg = await GetMsg(message.from_user.language_code, "save") #bot's reply 
+    if umsg in (db.getall_lc()):
+        await message.answer(msg)
+        await db.save_chat(message.chat.id, umsg) #write\update chat's language prefernce to db
+    else:
+        print("err: language does not exist in db, defaulting to english..")
+        print ()
+        await message.answer(msg)
+        await db.save_chat(message.chat.id,'en') #write defaulted chat's language prefernce to db
 ########### main logic end 
 
-#starter
+
+#starter################################################
 async def main():
     await bot.delete_webhook(drop_pending_updates=True) 
-    await database()
-    print ("bot initialized, started polling")
+    await init_db()
+    print ("starting polling...")
     await dp.start_polling(bot)
 
 if __name__=="__main__":
     asyncio.run(main())
-#starter
+#starter#################################################
